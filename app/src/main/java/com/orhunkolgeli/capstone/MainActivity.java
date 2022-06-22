8bc027b006a0e75e92b1c4a6fc869c753f83067c
Orhun Kolgeli <74283397+orhun-kolgeli@users.noreply.github.com>
2022-06-22 13:38:07 -0700

    Implement logout

--- app/src/main/java/com/orhunkolgeli/capstone/MainActivity.java
+++ app/src/main/java/com/orhunkolgeli/capstone/MainActivity.java
@@ -1,5 +1,6 @@
 package com.orhunkolgeli.capstone;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -21,6 +22,7 @@ import com.parse.ParseUser;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -59,16 +61,23 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        // Handle action bar item clicks
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_logout) {
+            ParseUser.logOutInBackground(e -> {
+                if(e != null) {
+                    Toast.makeText(MainActivity.this, "There was an error signing out.",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                // Prevent user from using back button to go back to Main Activity after logout
+                finish();
+                Toast.makeText(MainActivity.this, "Successfully logged out.", Toast.LENGTH_SHORT).show();
+            });
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
